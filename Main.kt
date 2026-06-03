f8c7fbdf7fde350c1c7e2d1d721408ac09756b0f
allanaxzs <allanabe415@gmail.com>
2026-06-03 08:10:51 -0300

    Add files via upload

--- Main.kt
+++ Main.kt
@@ -1,27 +1,35 @@
-package org.example
+import com.google.gson.Gson
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.util.Scanner
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val leitor = Scanner(System.`in`)
+    print("Digite um ID: ")
+    val buscador = leitor.nextInt()
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    val client: HttpClient  = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://pokeapi.co/api/v2/pokemon/$buscador"))
+        .build()
+    val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+    val dados = response.body()
+
+    val gson = Gson()
+    val resultado = runCatching {
+        val buscaApi = gson.fromJson(dados,  Informacoes::class.java)
+
+        val meuPokemon = Pokemon( buscaApi.name, buscaApi .abilities)
+
+        println(meuPokemon.nome.replaceFirstChar { it.uppercase() })
+        println("Habilidades: ")
+        for (i in meuPokemon.Habilidades){
+            println(i.ability.name.replaceFirstChar { it.uppercase() })
+        }
     }
+    resultado.onFailure{
+        print("Pokemon não encontrado!!")}
 }
-
-
-
-
-
-
-
-
-
-
-
